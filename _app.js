7ff231a9b1a1231bc1c91aa04116ff37c0cf6077
muhamadsholihuddin-rgb <muhamadsholihuddin@gmail.com>
2026-08-18 09:32:07 +0700

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch((err) => {
+        console.error("Gagal mendaftarkan service worker:", err);
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
